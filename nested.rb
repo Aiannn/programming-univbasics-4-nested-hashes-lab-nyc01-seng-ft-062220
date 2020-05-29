a0ef461d17ddd078d540a4e522a2de96c99324e8
Aian Soktoev <soktoevayan@gmail.com>
2020-05-29 23:10:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,8 +85,10 @@ def adding_matz
     }
   
   programmer_hash[:yukihiro_matsumoto] = {
-    
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
   }
+  programmer_hash
 end
 
 def changing_alan
@@ -130,5 +132,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+  
+  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+  programmer_hash
 end
